d0b7c3aa58b98e9e35df7392aae8d0210819fdfe
Joachim Øen <142489531+Potat001@users.noreply.github.com>
2026-03-15 02:47:48 +0100

    Finalized, background, calculator (mostly), JS Testing works, styling done!

--- index.js
+++ index.js
@@ -3,7 +3,15 @@
 //    input.value = input.value.replace(/[^0-9+\-*/.() ]/g, 'Math.sin Math.cos');
 //}
 window.onload = function() {
-    document.getElementById("display").value = '';};
+    document.getElementById("display").value = '';
+
+display.addEventListener("keydown", function(event) {
+    if(event.key === "Enter") {
+        event.preventDefault();
+        calculate();
+    }
+    });
+};
 
 const display = document.getElementById("display");
 
@@ -92,33 +100,39 @@ JSButton.addEventListener("mouseover", function() {
         case (hoverCount > 0 && hoverCount <= 2 && clickCount < 1):
             JSButton.style.animation = "moveButton 2s linear infinite alternate";
             JSButton.style.backgroundColor = "FireBrick";
+            JSButton.style.outline = "2px solid rgb(179, 115, 115)";
             JSButton.innerHTML = "Whoops, there it goes!";
-            JSButton.style.top = "150px";
+            /**
+            JSButton.style.top = "100px";
+            */
             catchButtonText.innerHTML = "Catch the button!!!";
             break;
         case (hoverCount >= 3 && hoverCount <= 9 && clickCount < 1):
             JSButton.style.animation = "moveButton 2s linear infinite alternate";
             JSButton.innerHTML = `That's ${hoverCount} attempts!`;
             JSButton.style.backgroundColor = "green";
+            JSButton.style.outline = "2px solid rgb(104, 133, 104)";
             catchButtonText.innerHTML = "Catch the button!!!";
             break;
         case (hoverCount >= 10 && hoverCount <= 39 && clickCount < 1):
             JSButton.style.animation = "moveButton 2s linear infinite alternate";
             JSButton.innerHTML = `That's ${hoverCount} attempts, come on!`;
             JSButton.style.backgroundColor = "DodgerBlue";
+            JSButton.style.outline = "2px solid rgb(135, 195, 255)";
             catchButtonText.innerHTML = "Catch the button!!!";
             break;
         case (hoverCount >= 30 && hoverCount <= 59 && clickCount < 1):
             JSButton.style.backgroundColor = "purple";
+            JSButton.style.outline = "2px solid rgb(128, 96, 128)";
             JSButton.innerHTML = `${hoverCount} attempts, you're got this!!!`
-
             JSButton.style.animation = "moveButton 2s linear infinite alternate";
             catchButtonText.innerHTML = "Catch the button!!!";
             break;
         case (hoverCount >= 60 && clickCount < 1):
             JSButton.innerHTML = `That's enough (${hoverCount}), here you go, click it!`
             JSButton.style.animation = "none";
-            JSButton.style.backgroundColor = "gray";
+            JSButton.style.background = "rgb(251, 0, 255)";
+            JSButton.style.outline = "2px solid rgb(253, 144, 255)";
             hoverCount = 60;
             break;
     }
@@ -132,7 +146,8 @@ JSButton.addEventListener("click", function() {
     if((hoverCount < 60 && clickCount > 0) || (hoverCount >= 60 && clickCount >= 1)) {
         JSButton.style.animation = "none";
         JSButton.innerHTML = `You caught me after ${hoverCount} hovers over the button!`;
-        JSButton.style.backgroundColor = "gray";
+        JSButton.style.background = "rgb(251, 0, 255)";
+        JSButton.style.outline = "2px solid rgb(253, 144, 255)";
         catchButtonText.style.marginLeft = "39%";
         catchButtonText.innerHTML = "You caught the button!!!";
         catchButton.style.marginLeft = "39%";
@@ -143,23 +158,28 @@ changeBackgroundButton.addEventListener("click", function() {
     console.log(`Clicked 2nd button ${clickCount2} times`)
     switch(true) {
         case(clickCount2 == 1):
-            document.documentElement.style.backgroundColor = "DodgerBlue";
+            document.documentElement.style.background = "linear-gradient(to right, rgb(48, 0, 0), rgb(29, 0, 0), rgb(0, 0, 0))";
+            document.querySelector("header").style.background = "linear-gradient(to right, rgb(48, 0, 0), rgb(29, 0, 0), rgb(0, 0, 0))";
             changeBackgroundButton.innerHTML = "Click me again!";
             break;
         case(clickCount2 == 2):
-            document.documentElement.style.backgroundColor = "FireBrick";
+            document.documentElement.style.background = "linear-gradient(to right, rgb(48, 45, 0), rgb(29, 29, 0),rgb(0, 0, 0))";
+            document.querySelector("header").style.background = "linear-gradient(to right, rgb(48, 45, 0), rgb(29, 29, 0),rgb(0, 0, 0))";
             changeBackgroundButton.innerHTML = "Aaaaand again!";
             break;
         case(clickCount2 == 3):
-            document.documentElement.style.backgroundColor = "green";
+            document.documentElement.style.background = "linear-gradient(to right, rgb(8, 48, 0), rgb(2, 29, 0), rgb(0, 0, 0))";
+            document.querySelector("header").style.background = "linear-gradient(to right, rgb(8, 48, 0), rgb(2, 29, 0), rgb(0, 0, 0))";
             changeBackgroundButton.innerHTML = "Again! :D";
             break;
         case(clickCount2 == 4):
-            document.documentElement.style.backgroundColor = "purple";
+            document.documentElement.style.background = "linear-gradient(to right, rgb(0, 30, 48), rgb(0, 15, 29),rgb(0, 0, 0))";
+            document.querySelector("header").style.background = "linear-gradient(to right, rgb(0, 30, 48), rgb(0, 15, 29),rgb(0, 0, 0))";
             changeBackgroundButton.innerHTML = "Ooooone last time!";
             break;
         case(clickCount2 >= 5):
-        document.documentElement.style.backgroundColor = "rgb(24, 24, 24)";
+        document.documentElement.style.background = "linear-gradient(to right, rgb(29, 0, 48), rgb(17, 0, 29), rgb(0, 0, 0))";
+        document.querySelector("header").style.background = "linear-gradient(to right, rgb(29, 0, 48), rgb(17, 0, 29), rgb(0, 0, 0))";
         changeBackgroundButton.innerHTML = "Aaaand back to normal!";
         break;
     }
